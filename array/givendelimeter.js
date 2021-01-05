04a1c8eae78ac602d044e593e74507d971e4bcbf
Metodi Angelov <sustavbg@gmail.com>
2021-01-05 17:53:24 +0200

    solution

---
+++ array/givendelimeter.js
@@ -0,0 +1,15 @@
+function givenDelimeter(arr) {
+    let delimeter = arr[arr.length - 1]
+    arr.pop()
+    let result = arr.join(`${delimeter}`)
+    return result
+}
+
+console.log(givenDelimeter(['How about no?', 
+'I',
+'will', 
+'not', 
+'do', 
+'it!', 
+'_']
+))
