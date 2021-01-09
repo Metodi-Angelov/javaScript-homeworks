5412f576b8028fe7683d3d8b2ef1b0d798fc27a0
Metodi Angelov <sustavbg@gmail.com>
2021-01-09 15:12:49 +0200

    new task

--- array/rotatearr.js
+++ array/rotatearr.js
@@ -1,6 +1,13 @@
-function rotateArr(arr) {
-    
+function rotateArr(arr = []) {
+    let amountRoration = +arr.pop();
+    const arrRotated = arr;
+    for (let i = 0; i < amountRoration; i++) {
+        arrRotated.unshift(arrRotated.pop());
+    }
+    console.log(arrRotated.join(' '));
 }
 
-rotateArr(['1', '2', '3', '4', '2'])
-rotateArr(['Banana', 'Orange', 'Coconut', 'Apple', '15'])
+
+
+rotateArr(['1', '2', '3', '4', '2']);
+rotateArr(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
