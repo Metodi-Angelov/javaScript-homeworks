4f54fdb2b639b90a2ddb581cd126101b19d59c1c
Metodi Angelov <sustavbg@gmail.com>
2021-01-25 20:54:26 +0200

    Solution

--- DOM exercise/02. Number-Convertor/solution.js
+++ DOM exercise/02. Number-Convertor/solution.js
@@ -1,14 +1,28 @@
 function solve() {
-    let optionList = document.getElementById('selectMenuTo');
-    let button = document.getElementsByTagName('button')[0];
-    button.addEventListener('click', (e) => {
-        console.log('clik')
-    })
-    optionList.innerHTML = 
-    `    
+    let optionList = document.querySelectorAll('#selectMenuTo')[0];
+    let button = document.querySelector('button');
+    let inputNumber = document.querySelector('#input');
+    let finalResult = document.querySelector('#result');
+
+
+    optionList.innerHTML =
+        `    
     <option selected value=""></option>
-        <option  value="binary">Binary</option>
-        <option  value="hexadecimal">Hexadeicmal</option>
+    <option  value="binary">Binary</option>
+    <option  value="hexadecimal">Hexadecimal</option>
     `
+
+    const options = { binary: 2, hexadecimal: 16 };
+
+    button.addEventListener('click', () => {
+        // let result
+        // if(optionList.value === 'binary') {
+        //    result = (+inputNumber.value).toString(2);
+        // } else {
+        //     result = (+inputNumber.value).toString(16);
+        // }
+        let result = (+inputNumber.value).toString(options[optionList.value])
+        finalResult.value = result;
+    })
 
 }
