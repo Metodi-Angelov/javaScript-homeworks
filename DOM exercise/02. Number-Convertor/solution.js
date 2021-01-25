de2f5eca597cfbe1d598f39a38c773ea9dc51bb8
Metodi Angelov <sustavbg@gmail.com>
2021-01-25 17:36:28 +0200

    Update solution.js

--- DOM exercise/02. Number-Convertor/solution.js
+++ DOM exercise/02. Number-Convertor/solution.js
@@ -1,4 +1,14 @@
 function solve() {
+    let optionList = document.getElementById('selectMenuTo');
+    let button = document.getElementsByTagName('button')[0];
+    button.addEventListener('click', (e) => {
+        console.log('clik')
+    })
+    optionList.innerHTML = 
+    `    
+    <option selected value=""></option>
+        <option  value="binary">Binary</option>
+        <option  value="hexadecimal">Hexadeicmal</option>
+    `
 
-    //TODO...
 }
